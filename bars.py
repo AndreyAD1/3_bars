efbe4a1a871a50de8b7cd9daffa067fbedba8192
Andrey <arakelyants.andrey@yandex.ru>
2018-04-12 10:38:38 +0300

    Improve the code format.

--- bars.py
+++ bars.py
@@ -10,60 +10,34 @@ def load_data(file_path='bars.json'):
 
 
 def get_biggest_bar(bars_info):
-    biggest_bar_info = max(bars_info, key=lambda x:
-                           x['properties']['Attributes']['SeatsCount']
-                           )
-    biggest_bar_name = biggest_bar_info['properties']['Attributes']['Name']
-    return biggest_bar_name
+    biggest_bar = max(
+        bars_info,
+        key=lambda x: x['properties']['Attributes']['SeatsCount']
+    )
+    return biggest_bar
 
 
 def get_smallest_bar(bars_info):
-    smallest_bar_info = min(bars_info, key=lambda x:
-                            x['properties']['Attributes']['SeatsCount']
-                            )
-    smallest_bar_name = smallest_bar_info['properties']['Attributes']['Name']
-    return smallest_bar_name
+    smallest_bar = min(
+        bars_info,
+        key=lambda x: x['properties']['Attributes']['SeatsCount']
+    )
+    return smallest_bar
 
 
-def get_bar_address(bar_features):
-    try:
-        bar_address = bar_features['properties']['Attributes']['Address']
-    except KeyError:
-        bar_address = None
-    return bar_address
-
-
-def get_distance(bar_info, longitude, latitude):
-    bar_longitude, bar_latitude = bar_info['geometry']['coordinates']
+def get_distance(bar, longitude, latitude):
+    bar_longitude, bar_latitude = bar['geometry']['coordinates']
     distance = math.hypot(bar_longitude - longitude,
                           bar_latitude - latitude)
     return distance
 
+
 def get_closest_bar(bar_data, longitude, latitude):
-    # closest_bar = min(bar_data, key=get_distance(__getitem__,longitude, latitude))
-    closest_bar = min(bar_data, key=lambda x: get_distance(x, longitude, latitude))
+    closest_bar = min(
+        bar_data,
+        key=lambda x: get_distance(x, longitude, latitude)
+    )
     return closest_bar
-    # first_loop = True
-    # closest_bar = None
-    # closest_bar_address = None
-    # minimal_distance = 0
-    # for bar in bar_data:
-    #     try:
-    #         coordinates = bar['geometry']['coordinates']
-    #     except KeyError:
-    #         continue
-    #     distance = math.hypot(coordinates[0] - longitude,
-    #                           coordinates[1] - latitude)
-    #     if first_loop:
-    #         minimal_distance = distance
-    #         closest_bar = bar['properties']['Attributes']['Name']
-    #         closest_bar_address = get_bar_address(bar)
-    #         first_loop = False
-    #     if distance < minimal_distance:
-    #         minimal_distance = distance
-    #         closest_bar = bar['properties']['Attributes']['Name']
-    #         closest_bar_address = get_bar_address(bar)
-    # return closest_bar, closest_bar_address
 
 
 def get_user_coordinates():
@@ -76,34 +50,35 @@ def get_user_coordinates():
 
 def get_console_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file',
-                        help='Если файл с данными называется не "bars.json"'
-                             ' и/или файл не лежит в папке скрипта, то '
-                             'используйте этот параметр, чтобы указать путь '
-                             'к файлу с данными о барах.'
-                        )
+    parser.add_argument(
+        '--file',
+        help='Если файл с данными называется не "bars.json" и/или файл не лежит'
+             ' в папке скрипта, то используйте этот параметр, чтобы указать'
+             ' путь к файлу с данными о барах.'
+    )
     args = parser.parse_args()
-    return args.file
+    return args
 
 
 if __name__ == '__main__':
-    path = 'bars.json'
-    user_path = get_console_arguments()
-    if user_path:
-        path = user_path
-    bar_info = load_data(path)
+    console_arguments = get_console_arguments()
+    user_file_path = console_arguments.file
+    if user_file_path:
+        bar_info = load_data(user_file_path)
+    else:
+        bar_info = load_data()
     bar_list = bar_info['features']
-    biggest_bar = get_biggest_bar(bar_list)
-    smallest_bar = get_smallest_bar(bar_list)
-    print('Самый большой бар Москвы ', biggest_bar)
-    print('Самый маленький бар Москвы: ', smallest_bar)
-    while True:
-        try:
-            lat, long = get_user_coordinates()
-            break
-        except ValueError:
-            print('Широта и долгота должны быть числами')
-            continue
+    biggest_bar_info = get_biggest_bar(bar_list)
+    smallest_bar_info = get_smallest_bar(bar_list)
+    biggest_bar_name = biggest_bar_info['properties']['Attributes']['Name']
+    smallest_bar_name = smallest_bar_info['properties']['Attributes']['Name']
+    print('Самый большой бар Москвы ', biggest_bar_name)
+    print('Самый маленький бар Москвы: ', smallest_bar_name)
+    try:
+        lat, long = get_user_coordinates()
+    except ValueError:
+        print('Широта и долгота должны быть числами')
+        raise SystemExit
     nearest_bar = get_closest_bar(bar_list, long, lat)
     nearest_bar_name = nearest_bar['properties']['Attributes']['Name']
     nearest_bar_address = nearest_bar['properties']['Attributes']['Address']
